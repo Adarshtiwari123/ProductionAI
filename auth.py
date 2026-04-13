4c4fa928cac99d683b08b0ae813654550ae87ad6
Adarsh tiwari <84695148+Adarshtiwari123@users.noreply.github.com>
2026-04-13 18:59:20 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,7 +3,15 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
+from passlib.context import CryptContext
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
